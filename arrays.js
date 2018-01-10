71e5d98024d55b45db8681931cb83cc9242d83ae
Taylor Lassiter <lassit09@gmail.com>
2018-01-10 02:57:34 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(array, element) {
-  return [(array, element), ...chocolateBars];
+  var chocolate = ["array, element", ...chocolateBars];
+  return chocolate;
 }
