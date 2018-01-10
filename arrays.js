0946bac7df85866f390e0b04f6da3e526d5ee870
Taylor Lassiter <lassit09@gmail.com>
2018-01-10 02:43:31 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,6 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  chocolateBars.unshift();
+  return chocolateBars;
+}
